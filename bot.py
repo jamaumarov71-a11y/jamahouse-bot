1be48c0a9f73ca69f32969e72855e5c1e06ccf69
jamaumarov71-a11y <jamaumarov71@gmail.com>
2026-06-12 01:35:19 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+TOKEN = "ВСТАВЬ_СЮДА_НОВЫЙ_ТОКЕН"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает ✅ Отправь ссылку OLX")
+
+async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if "olx" in text:
+        await update.message.reply_text(
+            "📦 OLX ссылка получена!\n\n"
+            "Скоро добавим:\n"
+            "✔ фото\n✔ описание\n✔ кнопка публикации"
+        )
+    else:
+        await update.message.reply_text("Отправь OLX ссылку")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, text_handler))
+
+print("Bot started")
+app.run_polling()
